9ae964c88cfb3c4196afd12e22e27322d5f4698c
Dedy Darmawan <dedywiradarmawan@gmail.com>
2024-01-06 23:30:54 +0800

    Created Demo Section
    Update:
    Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import {copy, linkIcon, loader, tick} from '../assets'
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className='mt-16 w-full max-w-xl'>
+      <div className='flex flex-col w-full gap-2'>
+        <form
+          className='relative flex justify-center items-center'
+          onSubmit={() => {}}
+        >
+          <img 
+            src={linkIcon} 
+            alt='link'
+            className='absolute my-2 left-0 ml-3 w-5' 
+          />
+
+          <input 
+            type='url'
+            placeholder='Enter a URL'
+            value=''
+            onChange={() => {}}
+            required
+            className='url_input peer' 
+          />
+
+          <button className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700' type='submit'>
+            ↵
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
